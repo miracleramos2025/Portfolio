7385d0624e82964d59c7c63f1a34570513b30a88
miracleramos2025 <miracleramos2025@u.northwestern.edu>
2026-02-26 14:28:29 -0600

    added button to view project section on homepage

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -25,16 +25,21 @@ export function Hero() {
           className="w-full h-full object-cover absolute inset-0"
         />
         <div className="absolute inset-0 flex flex-col items-start justify-start text-left pt-[4%] pl-[41%]">
-  <p style={{ color: "#0F2656" }} className="text-2xl font-light mb-2">
-    Hey, I'm
-  </p>
-  <h2 style={{ color: "#0F2656" }} className="text-5xl font-bold mb-4">
-    Miracle Ramos
-  </h2>
-  <p style={{ color: "#0F2656" }} className="text-2xl font-light">
-    I'm a <span ref={typedRef}></span>
-  </p>
-</div>
+          <p style={{ color: "#0F2656" }} className="text-2xl font-light mb-2">
+            Hey, I'm
+          </p>
+          <h2 style={{ color: "#0F2656" }} className="text-5xl font-bold mb-4">
+            Miracle Ramos
+          </h2>
+          <p style={{ color: "#0F2656" }} className="text-2xl font-light">
+            I'm a <span ref={typedRef}></span>
+          </p>
+          <a
+            href="#projects"
+            className="mt-5 text-white bg-[#0F2656] text-sm font-semibold px-6 py-2 rounded-full border border-[#0F2656] hover:bg-white hover:text-[#0F2656] transition-colors duration-200">
+            View My Work
+          </a>
+        </div>
       </div>
     </section>
   );
